af5b82d3406c31e220dcece5d11e2c44fbe55b86
Sergey <s.pavluchenko90@gmail.com>
2023-12-07 01:35:18 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,57 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+// console.log(list);
+
+const listImg = galleryItems
+  .map(
+    ({ description, original, preview }) => `
+    <li class='gallery-list'>
+        <a class='gallery-link' href="${original}">
+            <img class='gallery-image' src="${preview}" alt="${description}">
+        </a>
+    </li>
+`
+  )
+
+  .join(' ');
+
+list.insertAdjacentHTML('beforeend', listImg);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+//////////////////////
+
+// const list = document.querySelector('.gallery');
+// const allImg = murkup(galleryItems);
+
+// function murkup(items) {
+//   return items
+//     .map(
+//       ({ description, original, preview }) => `
+//     <li class='gallery-list'>
+//         <a class='gallery-link' href="${original}">
+//             <img class='gallery-image' src="${preview}" alt="${description}">
+//         </a>
+//     </li>
+// `
+//     )
+//     .join(' ');
+// }
+
+// list.insertAdjacentHTML('beforeend', allImg);
+
+// let lightbox = new SimpleLightbox('.gallery a', {
+//   captionDelay: 250,
+//   captionsData: 'alt',
+// });
