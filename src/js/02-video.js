8252df2fa583132a6e9c3df032f9e2d11a97d4d3
Sergey <s.pavluchenko90@gmail.com>
2023-12-11 22:49:28 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+console.log(iframe);
+
+const player = new Player(iframe);
+console.log(player);
+
+const onPlay = function (currntTime) {
+  console.log(currntTime.seconds);
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(currntTime.seconds)
+  );
+};
+
+const getTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+console.log(getTime);
+
+player.setCurrentTime(getTime || 0);
+
+player.on('timeupdate', throttle(onPlay, 1000));
